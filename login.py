db18eee6c82b3b5e10a7b9959204522bd3cea8fb
rocketbunny22 <65988765+rocketbunny22@users.noreply.github.com>
2023-09-13 11:55:15 -0400

    Update login.py

--- login.py
+++ login.py
@@ -2,7 +2,8 @@ import bcrypt
 hashed_passwd = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
 max_attempts = 3
 attempts = 0
-user_database = {'user1' : 'password1', 'user2' : 'password2'}
+user_database = {'user1': bcrypt.hashpw('password1'.encode('utf-8'), bcrypt.gensalt()),
+                 'user2': bcrypt.hashpw('password2'.encode('utf-8'), bcrypt.gensalt())}
 while attempts < max_attempts:
  username = input("Enter your username:")
  password = input("Enter your password:")
