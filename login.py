6d9f4bfad6dc8ebd14a469b1a50cb4145342c88a
rocketbunny22 <65988765+rocketbunny22@users.noreply.github.com>
2023-09-13 11:48:21 -0400

    Update login.py

--- login.py
+++ login.py
@@ -1,3 +1,5 @@
+import bcrypt
+hashed_passwd = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
 max_attempts = 3
 attempts = 0
 user_database = {'user1' : 'password1', 'user2' : 'password2'}
@@ -5,7 +7,7 @@ while attempts < max_attempts:
  username = input("Enter your username:")
  password = input("Enter your password:")
 
- if username in user_database and user_database[username] == password:
+ if username in user_database and bcrypt.checkpw(password.encode('utf-8'), user_database[username] == password:
     print("Login successful! Welcome, " + username + "!")
     break
  else:
