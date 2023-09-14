301d838f73fd53a5cb5bc849056f22b6385fe152
rocketbunny22 <65988765+rocketbunny22@users.noreply.github.com>
2023-09-14 16:42:46 -0400

    Add files via upload

---
+++ hw.py
@@ -0,0 +1,8 @@
+person=(input("Name please: "))
+age=(int(input("Please enter your age: ")))
+if person == "lori" and age == 27:
+    print("Welcome! "+"\nWhy you so cute?")
+else:
+    print("go away, you ugly.")    
+
+  
